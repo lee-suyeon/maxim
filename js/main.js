07c98429f74277644e5786a2c9d192c55b4f7540
sroovy <42996064+sroovy@users.noreply.github.com>
2020-03-10 16:18:40 +0900

    20200310

--- js/main.js
+++ js/main.js
@@ -8,22 +8,24 @@ $(function () {
             $(this).find(".dropdown").stop().slideUp(200);
         });
 
-
+    
     //배너 슬라이드
         var $container = $(".slideshow"),
             slideGroup = $container.find(".slideshow_slides"),
             banner = slideGroup.children("div");
 
         var pagination = $container.find(".current"),
-            barWidth = pagination.outerWidth(),
+
             nav = $container.find(".slideshow_nav"),
             bannerCount = banner.length;
-            
+
+
         var currentIndex = 0;   
         var timer;
 
         //슬라이드 이동 함수
         function goToSlide(index){
+            barWidth = pagination.outerWidth();
             slideGroup.stop().animate({left: -100 * index + "%"});
             pagination.stop().animate({left: barWidth * index + "px"});
             currentIndex = index;
@@ -71,4 +73,59 @@ $(function () {
         }, function () {
             timer = window.setInterval(nextSlide, 3000);
         });
+
+        
+    // ABOUT scroll evnet
+        // 스크롤의 위치가 일정 순간에 오면
+        // 슬로건 메뉴가 나타난다. 
+        
+        var $slogan = $(".about .slogan");
+        var aboutPos = $(".about h3").offset().top;
+
+        $(window).scroll(function () {
+            var scrollTop = $(window).scrollTop();
+            if(scrollTop >= aboutPos - 500){
+                $slogan.addClass("show");
+            };
+        });
+
+
+    // news hover
+    var $newsContents = $(".news .news_contents > div");
+
+   $newsContents.hover(function () {
+       $(this).find(".contents_title").css({
+            "background":"#e94d50",
+            "color":"#fff"});
+       $(this).find(".overlay").css({"display":"block"});
+       $(this).find("img").css({"transform":"scale(1.05)"});
+   },function () {
+        $(this).find(".contents_title").css({
+            "background":"none",
+            "color":"#e94d50"});
+        $(this).find(".overlay").css({"display":"none"});
+        $(this).find("img").css({"transform":"scale(1)"});
+   });
+
+
+   // notice rolling
+   var $container = $(".notice_rolling"),
+        rolling = $container.find(".rolling");
+
+    var rollingTimer;
+
+    rollingTimer = window.setInterval(doRolling, 2000);
+
+    function doRolling () {
+        rolling.css({"transition":"0.4s",top:"-2em"});
+
+        window.setTimeout(function () {
+            rolling.removeAttr("style");
+        }, 400);
+        
+    }
+
+console.log(rolling.children().first());
+
+
 });
